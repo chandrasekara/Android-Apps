31bff2f71d4a7f5a992336e9a3224e1d7acc8053
chandrasekara <dchandraseka@student.unimelb.edu.au>
2017-12-03 18:21:21 +1100

    conversion functionality added

--- currency_converter/app/src/main/java/com/example/dhila/currencyconverter/MainActivity.java
+++ currency_converter/app/src/main/java/com/example/dhila/currencyconverter/MainActivity.java
@@ -2,8 +2,26 @@ package com.example.dhila.currencyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void conversion(View view) {
+
+        EditText myTextView = (EditText) findViewById(R.id.editText);
+
+        Log.i("Test", "Button clicked");
+
+        double USD_AUD = 1.314;
+
+        Toast.makeText(MainActivity.this, Double.toString(Integer.parseInt(myTextView.getText().toString()) * USD_AUD), Toast.LENGTH_LONG
+        ).show();
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
