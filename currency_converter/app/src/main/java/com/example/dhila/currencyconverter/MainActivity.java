a8c99c8959f342d2730a1d7eab9b98e8c1d2645d
chandrasekara <dchandraseka@student.unimelb.edu.au>
2017-12-04 19:37:39 +1100

    tidy up and comment currency conversion java code

--- currency_converter/app/src/main/java/com/example/dhila/currencyconverter/MainActivity.java
+++ currency_converter/app/src/main/java/com/example/dhila/currencyconverter/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.dhila.currencyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -11,13 +10,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void conversion(View view) {
 
-        EditText myEditText = (EditText) findViewById(R.id.editText);
+        //Convert the input to an EditText
+        EditText myEditText = (EditText) findViewById(R.id.usd_input);
 
+        //Incrementally calculate the AUD amount from the USD amount, using appropriate data type
+        //conversions
         double USD_AUD = 1.314;
         String editTextToString = myEditText.getText().toString();
         int editTextToInt = Integer.parseInt(editTextToString);
         double convertedAmount = USD_AUD * editTextToInt;
 
+
+        //Output of the converter
         Toast.makeText(MainActivity.this, Double.toString(convertedAmount), Toast.LENGTH_LONG
         ).show();
 
