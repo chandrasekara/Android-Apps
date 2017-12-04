95e2bf9e426a4b8207b5a4c5fbaddfe647854aff
chandrasekara <dchandraseka@student.unimelb.edu.au>
2017-12-04 16:21:09 +1100

    add working square number check functionality

--- mini_apps/number_shapes/app/src/main/java/com/example/dhila/numbershapes/MainActivity.java
+++ mini_apps/number_shapes/app/src/main/java/com/example/dhila/numbershapes/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.dhila.numbershapes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.lang.Math;
+
+import static java.lang.Math.sqrt;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,18 +17,34 @@ public class MainActivity extends AppCompatActivity {
 
         //first test whether it is empty or not
 
+        boolean isSquareNumberValue = false;
+        boolean isTriangularNumberValue = false;
+
         EditText myNumberInput = (EditText) findViewById(R.id.numberInput);
 
         if (myNumberInput.getText().toString().trim().length() > 0) {
 
-            Toast.makeText(MainActivity.this, "not empty", Toast.LENGTH_LONG
-            ).show();
+            isSquareNumberValue = isSquareNumber(Integer.parseInt(myNumberInput.getText()
+                    .toString()));
+            Toast.makeText(MainActivity.this, String.valueOf(isSquareNumberValue),
+                    Toast.LENGTH_LONG).show();
         }
         else {
             Toast.makeText(MainActivity.this, "empty", Toast.LENGTH_LONG
             ).show();
         }
 
+    }
+
+    public boolean isSquareNumber(int inputNum) {
+
+        double squareLeftOver = sqrt(inputNum) - (int)sqrt(inputNum);
+
+        if (squareLeftOver > 0.0) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     @Override
