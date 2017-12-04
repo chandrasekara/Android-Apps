d103783c20ec3520b16d73059feaccdda6a6d3f5
chandrasekara <dchandraseka@student.unimelb.edu.au>
2017-12-04 15:52:00 +1100

    add check for empty input box

--- mini_apps/number_shapes/app/src/main/java/com/example/dhila/numbershapes/MainActivity.java
+++ mini_apps/number_shapes/app/src/main/java/com/example/dhila/numbershapes/MainActivity.java
@@ -2,8 +2,30 @@ package com.example.dhila.numbershapes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void testNumber(View view) {
+
+        //first test whether it is empty or not
+
+        EditText myNumberInput = (EditText) findViewById(R.id.numberInput);
+
+        if (myNumberInput.getText().toString().trim().length() > 0) {
+
+            Toast.makeText(MainActivity.this, "not empty", Toast.LENGTH_LONG
+            ).show();
+        }
+        else {
+            Toast.makeText(MainActivity.this, "empty", Toast.LENGTH_LONG
+            ).show();
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
