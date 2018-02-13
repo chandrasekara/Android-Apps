c7d212f3de464f821b2bcb32755b2fd7076d23f8
Dhilan Chandrasekara <dchandraseka@student.unimelb.edu.au>
2018-02-13 12:26:35 +1100

    fix: resolve crashing issue for certain values of tube creation delay counter

--- FlappyBird_clone/core/src/com/mygdx/flappybird/TubeGenerator.java
+++ FlappyBird_clone/core/src/com/mygdx/flappybird/TubeGenerator.java
@@ -78,7 +78,7 @@ public class TubeGenerator {
             FPS = 80; //magic numbers, change later
         }
 
-        if (counter > FPS * 1.2) {
+        if (counter > (int)(FPS * 1.9)) {
 
             this.game.addTubePair( generateTube()  );
 
